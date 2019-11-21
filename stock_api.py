578f32319f13cdaa236f36c94580c253c1161dff
Basil Latif <blatif@ranker.com>
2019-11-21 14:06:49 -0800

    Data from 07

--- stock_api.py
+++ stock_api.py
@@ -7,44 +7,44 @@ from bs4 import BeautifulSoup
 #All of FB data-Get recent 2019 fb data
 headers = []
 def fb_data():
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/FB/data.csv?start_date=2012-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/FB/data.csv?start_date=2007-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
     headers = {"Date":"Open" , "High":"Low", "Close":"Volume", "Ex-Dividend":"Split Ratio", "Adj.":"Open,Adj.", "High,Adj.":"Low,Adj.", "Close,Adj.":"Volume"}
     result = requests.get(url, headers = headers)
     fb_scrape = result.content
-    f = open('/Users/basillatif/Desktop/Stock Data Scrape/fb_data.csv', "w")
+    f = open('/Users/basillatif/Desktop/Stock Data Project/fb_data.csv', "w")
     f.write(result.text)
     f.close()
 def aapl_data():
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/AAPL/data.csv?start_date=2012-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/AAPL/data.csv?start_date=2007-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
     headers = {"Date":"Open" , "High":"Low", "Close":"Volume", "Ex-Dividend":"Split Ratio", "Adj.":"Open,Adj.", "High,Adj.":"Low,Adj.", "Close,Adj.":"Volume"}
     result = requests.get(url, headers = headers)
     aapl_scrape = result.content
-    f = open('/Users/basillatif/Desktop/Stock Data Scrape/aapl_data.csv', "w")
+    f = open('/Users/basillatif/Desktop/Stock Data Project/aapl_data.csv', "w")
     f.write(result.text)
     f.close()
 def amzn_data():
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/AMZN/data.csv?start_date=2012-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/AMZN/data.csv?start_date=2007-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
     headers = {"Date":"Open" , "High":"Low", "Close":"Volume", "Ex-Dividend":"Split Ratio", "Adj.":"Open,Adj.", "High,Adj.":"Low,Adj.", "Close,Adj.":"Volume"}
     result = requests.get(url, headers = headers)
     amzn_scrape = result.content
-    f = open('/Users/basillatif/Desktop/Stock Data Scrape/amzn_data.csv', "w")
+    f = open('/Users/basillatif/Desktop/Stock Data Project/amzn_data.csv', "w")
     f.write(result.text)
     f.close()
 def nflx_data():
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/NFLX/data.csv?start_date=2012-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/NFLX/data.csv?start_date=2007-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
     headers = {"Date":"Open" , "High":"Low", "Close":"Volume", "Ex-Dividend":"Split Ratio", "Adj.":"Open,Adj.", "High,Adj.":"Low,Adj.", "Close,Adj.":"Volume"}
     result = requests.get(url, headers = headers)
     nflx_scrape = result.content
-    f = open('/Users/basillatif/Desktop/Stock Data Scrape/nflx_data.csv', "w")
+    f = open('/Users/basillatif/Desktop/Stock Data Project/nflx_data.csv', "w")
     f.write(result.text)
     f.close()
 def googl_data():
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/GOOGL/data.csv?start_date=2012-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
+    url = 'https://www.quandl.com/api/v3/datasets/WIKI/GOOGL/data.csv?start_date=2007-11-01&end_date=2019-11-01&api_key=e4GhL94a1or42eyT5bTz'
     headers = {"Date":"Open" , "High":"Low", "Close":"Volume", "Ex-Dividend":"Split Ratio", "Adj.":"Open,Adj.", "High,Adj.":"Low,Adj.", "Close,Adj.":"Volume"}
     result = requests.get(url, headers = headers)
     googl_scrape = result.content
     print(googl_scrape)
-    f = open('/Users/basillatif/Desktop/Stock Data Scrape/googl_data.csv', "w")
+    f = open('/Users/basillatif/Desktop/Stock Data Project/googl_data.csv', "w")
     f.write(result.text)
     f.close()
 
